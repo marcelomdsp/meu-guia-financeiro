396c30fa8624b8c60f1a858b267387331c14df19
marcelomdsp <marcelopaulomsp@gmail.com>
2025-04-16 01:50:37 -0300

    Correçao no header

--- js/script.js
+++ js/script.js
@@ -32,32 +32,3 @@ busca.addEventListener('input', () => {
   }
 });
 
-
-
-
-/*const busca = document.getElementById('busca');
-const produtos = document.querySelectorAll('.produto');
-const mensagemVazia = document.getElementById('mensagem-vazia');
-
-busca.addEventListener('input', () => {
-  const termo = busca.value.toLowerCase();
-  let encontrados = 0;
-
-  produtos.forEach(produto => {
-    const titulo = produto.querySelector('h2').textContent.toLowerCase();
-    const descricao = produto.querySelector('p').textContent.toLowerCase();
-    const corresponde = titulo.includes(termo) || descricao.includes(termo);
-
-    produto.classList.toggle('oculto', !corresponde);
-    if (corresponde) encontrados++;
-  });
-
-
-
-  if (encontrados === 0) {
-    mensagemVazia.classList.add('visivel');
-  } else {
-    mensagemVazia.classList.remove('visivel');
-  }
-   
-});*/
